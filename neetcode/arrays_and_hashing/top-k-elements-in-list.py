dc1cec83050c4ece14c8af6dd00b442592931d52
Jagdish Tirumala <t.jag587@gmail.com>
2026-08-19 17:18:18 +0530

    enhanced arrays

--- neetcode/arrays_and_hashing/top-k-elements-in-list.py
+++
@@ -1,6 +0,0 @@
-# https://neetcode.io/problems/top-k-elements-in-list
-
-class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        values = Counter(nums).most_common(k)
-        return [value[0] for value in values]
--- neetcode/arrays_and_hashing/valid_anagram.py
+++
@@ -1,7 +0,0 @@
-#https://neetcode.io/problems/is-anagram
-
-from collections import Counter
-class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        return Counter(s) == Counter(t)
-        
